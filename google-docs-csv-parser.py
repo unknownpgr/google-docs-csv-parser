f5a41ce95d142921bf00b22b04f927725a20cb08
unknownpgr <unknownpgr@gmail.com>
2020-10-13 00:53:32 +0900

    Add some project specific function

--- google-docs-csv-parser.py
+++ google-docs-csv-parser.py
@@ -2,20 +2,27 @@ import os
 import csv
 import re
 
-for f in os.listdir('./'):
-    if not f.endswith('.txt'):
+for d in os.listdir('./'):
+    if os.path.isfile(d):
         continue
-    print(f)
-    writer = csv.writer(open(f+'.csv', 'w', encoding='utf-8-sig', newline=''))
-    row = ''
-    annot = False
-    for line in open(f, 'r', encoding='utf-8'):
-        line = line.replace('* ', '').strip()
-        if line.startswith('[') or annot:
-            annot = True
-            if line.startswith('['):
-                if len(row) > 2:
-                    writer.writerow([row.strip()])
-                row = line
-            else:
-                row += line+'\n'
+    if d.startswith('.'):
+        continue
+    writer = csv.writer(open(d+'.csv', 'w', encoding='utf-8-sig', newline=''))
+    for f in os.listdir(d):
+        if not f.endswith('.txt'):
+            continue
+        p = re.search('P[0-9]+', f).group(0)
+        print(d, p)
+        row = ''
+        annot = False
+        for line in open(d+'/'+f, 'r', encoding='utf-8'):
+            line = line.replace('* ', '').strip()
+            if line.startswith('[') or annot:
+                annot = True
+                if line.startswith('['):
+                    if len(row) > 2:
+                        writer.writerow(
+                            [p, d, row.strip()])
+                    row = line
+                else:
+                    row += line+'\n'
