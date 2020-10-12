99628f607064408be691498ab9f62114d9c33bb1
unknownpgr <unknownpgr@gmail.com>
2020-10-12 19:47:44 +0900

    Fix some error

--- google-docs-csv-parser.py
+++ google-docs-csv-parser.py
@@ -1,10 +1,21 @@
 import os
 import csv
+import re
 
 for f in os.listdir('./'):
     if not f.endswith('.txt'):
         continue
+    print(f)
     writer = csv.writer(open(f+'.csv', 'w', encoding='utf-8-sig', newline=''))
+    row = ''
+    annot = False
     for line in open(f, 'r', encoding='utf-8'):
-        if line.startswith('['):
-            writer.writerow([line.replace("\r","").replace('\n','')])
+        line = line.replace('* ', '').strip()
+        if line.startswith('[') or annot:
+            annot = True
+            if line.startswith('['):
+                if len(row) > 2:
+                    writer.writerow([row.strip()])
+                row = line
+            else:
+                row += line+'\n'
